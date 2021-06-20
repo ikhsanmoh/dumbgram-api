829b0ba2c0bcf61abaff4925087e11bf57553e00
Ikhsan Mohamad <icanmohamad13@gmail.com>
2021-06-21 01:01:43 +0700

    Add new Dependencies for FE-BE Integration

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const routers = require('./src/routers')
+const cors = require('cors');
+
 const app = express()
 
 // use Body Parser
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/v1', routers)
 
